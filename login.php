0d2316185d41ac4b191bc340a5f07b3626833470
Sauby <imsauby@gmail.com>
2024-04-04 21:19:46 +0530

    added php login functionality

--- login.php
+++ login.php
@@ -15,12 +15,64 @@
 
         <small>Password</small>
         <input type="password" name="password" />
+        <button type="submit">Submit</button>
     </form>
+
     <?php
     $email = $_POST["email"];
     $passwd = $_POST["password"];
 
-    echo "$email $passwd";
+    // function loginUser($femail, $fpasswd)
+    // {
+    //     try {
+    //         $file = fopen("users.txt", "r");
+
+    //         $data = file_get_contents("users.txt");
+    //         $dataInArr = explode("\n", $data);
+
+    //         foreach ($dataInArr as $user) {
+    //             echo $user;
+
+    //             $userArr = explode("-", $user);
+    //             if ($femail == $userArr[1]) {
+    //                 if ($fpasswd == $userArr[2]) echo $user;
+    //             }
+    //         }
+    //         fclose($file);
+    //     } catch (\Throwable $th) {
+    //         throw $th;
+    //     }
+    // }
+
+    // if ($email != "" || $passwd != "") loginUser($email, $passwd);
+
+    function loginUser($femail, $fpasswd)
+{
+    try {
+        $file = fopen("users.txt", "r");
+
+        while (!feof($file)) {
+            $line = trim(fgets($file));
+
+            if (!empty($line)) {
+                $userArr = explode("-", $line);
+                if ($femail == $userArr[1] && $fpasswd == $userArr[2]) {
+                    fclose($file);
+                    return true; 
+                }
+            }
+        }
+        fclose($file);
+        return false;
+    } catch (\Throwable $th) {
+        throw $th;
+    }
+}
+
+if ($email != "" && $passwd != "") {
+    if (loginUser($email, $passwd)) echo "Login successful!";
+    else echo "Invalid email or password.";
+}
     ?>
 
 </body>
